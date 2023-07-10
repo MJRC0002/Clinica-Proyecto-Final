8b959ea9a86260a098ce54d4cb3f475a5142f778
DELL <DELL@SaratrasV>
2023-07-10 17:33:30 -0400

    Creando vacuna.

---
+++ Clinica Proyecto Final/src/logico/Vacuna.java
@@ -0,0 +1,24 @@
+package logico;
+
+public class Vacuna {
+	private String codigo;
+	private String nombre;
+	private String descripcion;
+	public Vacuna(String codigo, String nombre, String descripcion) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+
+}
