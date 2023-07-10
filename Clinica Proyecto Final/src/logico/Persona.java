9bc82dba97f9c8799e488a94e361c608342c2596
manue <manue@MSI>
2023-07-10 17:13:51 -0400

    Actualizando la clase Persona.

--- Clinica Proyecto Final/src/logico/Persona.java
+++ Clinica Proyecto Final/src/logico/Persona.java
@@ -2,6 +2,48 @@ package logico;
 
 public class Persona {
 	private String codigo;
-	private String nombre; 
+	private String nombre;
+	private int edad;
+	private boolean seguroMedico;
+
+	public Persona(String codigo, String nombre, int edad, boolean seguroMedico) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.seguroMedico = seguroMedico;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public boolean isSeguroMedico() {
+		return seguroMedico;
+	}
+
+	public void setSeguroMedico(boolean seguroMedico) {
+		this.seguroMedico = seguroMedico;
+	}
 
 }
