7beb3e70aebc7afbad11ac17412478683ea1e1ec
DELL <DELL@192.168.56.1>
2023-07-17 15:37:42 -0400

    Modificando Clinica y RegCita

--- Clinica Proyecto Final/src/visual/RegCita.java
+++ Clinica Proyecto Final/src/visual/RegCita.java
@@ -78,28 +78,29 @@ public class RegCita extends JDialog {
 			panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			contentPanel.add(panel, BorderLayout.CENTER);
 			panel.setLayout(null);
-			
+
 			JPanel panelCita = new JPanel();
-			panelCita.setBorder(new TitledBorder(null, "Informaci\u00F3n de cita:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelCita.setBorder(new TitledBorder(null, "Informaci\u00F3n de cita:", TitledBorder.LEADING,
+					TitledBorder.TOP, null, null));
 			panelCita.setBounds(15, 16, 489, 142);
 			panel.add(panelCita);
 			panelCita.setLayout(null);
-			
+
 			JLabel lblCodigoCita = new JLabel("C\u00F3digo:");
 			lblCodigoCita.setBounds(15, 40, 69, 20);
 			panelCita.add(lblCodigoCita);
-			
+
 			txtCodigoCita = new JTextField();
-			txtCodigoCita.setText("Cita - "+Clinica.getInstance().codigoCita);
+			txtCodigoCita.setText("Cita - " + Clinica.getInstance().codigoCita);
 			txtCodigoCita.setBounds(73, 37, 146, 26);
 			panelCita.add(txtCodigoCita);
 			txtCodigoCita.setEditable(false);
 			txtCodigoCita.setColumns(10);
-			
+
 			JLabel lblCodigoDoctor = new JLabel("C\u00F3digo del doctor:");
 			lblCodigoDoctor.setBounds(15, 79, 139, 20);
 			panelCita.add(lblCodigoDoctor);
-			
+
 			txtCodigoDoctor = new JTextField();
 			txtCodigoDoctor.addKeyListener(new KeyAdapter() {
 				@Override
@@ -112,22 +113,21 @@ public class RegCita extends JDialog {
 			txtCodigoDoctor.setBounds(151, 79, 146, 26);
 			panelCita.add(txtCodigoDoctor);
 			txtCodigoDoctor.setColumns(10);
-			
+
 			JLabel lblFecha = new JLabel("Fecha:");
 			lblFecha.setBounds(244, 40, 53, 20);
 			panelCita.add(lblFecha);
-			
+
 			spnFecha = new JSpinner();
 			spnFecha.setBounds(294, 37, 155, 26);
 			panelCita.add(spnFecha);
 			spnFecha.setModel(new SpinnerDateModel(new Date(), null, null, Calendar.DAY_OF_YEAR));
-			
+
 			JButton btnBuscar = new JButton("Buscar");
 			btnBuscar.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					Medico medico = Clinica.getInstance().buscarMedicoByCode(txtCodigoDoctor.getText());
-					if( medico != null)
-					{
+					if (medico != null) {
 						txtCodigoPersona.setEnabled(true);
 						txtEdad.setEnabled(true);
 						txtNombre.setEnabled(true);
@@ -136,66 +136,67 @@ public class RegCita extends JDialog {
 			});
 			btnBuscar.setBounds(304, 78, 115, 29);
 			panelCita.add(btnBuscar);
-			
+
 			panelPersona = new JPanel();
-			panelPersona.setBorder(new TitledBorder(null, "Informaci\u00F3n de persona:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelPersona.setBorder(new TitledBorder(null, "Informaci\u00F3n de persona:", TitledBorder.LEADING,
+					TitledBorder.TOP, null, null));
 			panelPersona.setBounds(15, 194, 489, 158);
 			panel.add(panelPersona);
 			panelPersona.setLayout(null);
-			
+
 			JLabel lblCodigoPersona = new JLabel("C\u00F3digo:");
 			lblCodigoPersona.setBounds(15, 36, 57, 20);
 			panelPersona.add(lblCodigoPersona);
-			
+
 			txtCodigoPersona = new JTextField();
 			txtCodigoPersona.setEnabled(false);
 			txtCodigoPersona.setColumns(10);
 			txtCodigoPersona.setBounds(72, 33, 146, 26);
 			panelPersona.add(txtCodigoPersona);
-			
+
 			JLabel lblNombre = new JLabel("Nombre:");
 			lblNombre.setBounds(15, 75, 76, 20);
 			panelPersona.add(lblNombre);
-			
+
 			txtNombre = new JTextField();
 			txtNombre.setEnabled(false);
 			txtNombre.setColumns(10);
 			txtNombre.setBounds(83, 72, 146, 26);
 			panelPersona.add(txtNombre);
-			
+
 			lblEdad = new JLabel("Edad:");
 			lblEdad.setBounds(255, 36, 57, 20);
 			panelPersona.add(lblEdad);
-			
+
 			txtEdad = new JTextField();
 			txtEdad.setEnabled(false);
 			txtEdad.setColumns(10);
 			txtEdad.setBounds(304, 33, 146, 26);
 			panelPersona.add(txtEdad);
-			
+
 			lblSeguroMedico = new JLabel("Seguro M\u00E9dico:");
 			lblSeguroMedico.setBounds(255, 75, 117, 20);
 			panelPersona.add(lblSeguroMedico);
-			
+
 			rdbtnTieneSeguro = new JRadioButton("si");
 			rdbtnTieneSeguro.setEnabled(false);
 			rdbtnTieneSeguro.setBounds(365, 71, 57, 29);
 			panelPersona.add(rdbtnTieneSeguro);
-			
+
 			rdbtnNoTieneSeguro = new JRadioButton("No");
 			rdbtnNoTieneSeguro.setEnabled(false);
 			rdbtnNoTieneSeguro.setBounds(424, 71, 65, 29);
 			panelPersona.add(rdbtnNoTieneSeguro);
-			
+
 			lblGenero = new JLabel("G\u00E9nero:");
 			lblGenero.setBounds(15, 121, 86, 20);
 			panelPersona.add(lblGenero);
-			
+
 			rdbtnMasculino = new JRadioButton("M");
 			rdbtnMasculino.setEnabled(false);
 			rdbtnMasculino.setBounds(94, 117, 57, 29);
 			panelPersona.add(rdbtnMasculino);
-			
+
 			rdbtnFemenino = new JRadioButton("F");
 			rdbtnFemenino.setEnabled(false);
 			rdbtnFemenino.setBounds(153, 117, 65, 29);
@@ -210,16 +211,19 @@ public class RegCita extends JDialog {
 				btnRegistrar = new JButton("Registrar");
 				btnRegistrar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						boolean tieneSeguro;
-						if(rdbtnTieneSeguro.isSelected())
-							tieneSeguro = true;
-						else 
-							tieneSeguro = false;
-						
-						Persona persona = new Persona(txtCodigoPersona.getText(), txtNombre.getText(), Integer.parseInt(txtEdad.getText()), tieneSeguro);
-						Cita cita = new Cita(txtCodigoCita.getText(), txtCodigoDoctor.getText(), persona);
-						Clinica.getInstanceSecretaria().getMisCitas().add(cita);
-						Cleaner();
+						if (Clinica.miSecretaria != null) {
+							boolean tieneSeguro;
+							if (rdbtnTieneSeguro.isSelected())
+								tieneSeguro = true;
+							else
+								tieneSeguro = false;
+
+							Persona persona = new Persona(txtCodigoPersona.getText(), txtNombre.getText(),
+									Integer.parseInt(txtEdad.getText()), tieneSeguro);
+							Cita cita = new Cita(txtCodigoCita.getText(), txtCodigoDoctor.getText(), persona);
+							Clinica.miSecretaria.getMisCitas().add(cita);
+							Cleaner();
+						}
 					}
 				});
 				btnRegistrar.setEnabled(false);
