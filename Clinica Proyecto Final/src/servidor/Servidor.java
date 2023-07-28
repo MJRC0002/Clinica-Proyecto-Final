a7da4b3503679cc5820b697fe30977abae6cf355
DELL <DELL@SaratrasV>
2023-07-28 11:54:00 -0400

    Soluccionando error de guardado en el clase Servidor

--- Clinica Proyecto Final/src/servidor/Servidor.java
+++ Clinica Proyecto Final/src/servidor/Servidor.java
@@ -4,71 +4,74 @@ import java.io.*;
 import java.net.*;
 
 public class Servidor extends Thread {
-	private static int connectionCounter = 1;
-	private static boolean running = true;
+    private static int connectionCounter = 1;
+    private static boolean running = true;
 
-	public static void main(String args[]) {
-		ServerSocket sfd = null;
-		try {
-			sfd = new ServerSocket(8007);
-			System.out.println("Servidor listo y esperando conexiones...");
-		} catch (IOException ioe) {
-			System.out.println("Comunicación rechazada." + ioe);
-			System.exit(1);
-		}
+    public static void main(String args[]) {
+        ServerSocket sfd = null;
+        try {
+            sfd = new ServerSocket(8016);
+            System.out.println("Servidor listo y esperando conexiones...");
 
-		// Agregar un ShutdownHook para cerrar el servidor de forma adecuada
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			running = false;
-			try {
-				sfd.close();
-				System.out.println("Servidor cerrado.");
-			} catch (IOException e) {
-				System.out.println("Error al cerrar el servidor: " + e.getMessage());
-			}
-		}));
+            // Declarar sfd fuera del bloque try
+            final ServerSocket finalSfd = sfd;
 
-		while (running) {
-			try {
-				Socket nsfd = sfd.accept();
-				System.out.println("Conexion aceptada de: " + nsfd.getInetAddress());
-				new Thread(() -> {
-					handleClient(nsfd);
-				}).start();
-			} catch (IOException ioe) {
-				System.out.println("Error: " + ioe);
-			}
-		}
-	}
+            // Registrar un ShutdownHook para cerrar el servidor de forma adecuada
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    running = false;
+                    try {
+                        if (finalSfd != null)
+                            finalSfd.close();
+                        System.out.println("Servidor cerrado.");
+                    } catch (IOException e) {
+                        System.out.println("Error al cerrar el servidor: " + e.getMessage());
+                    }
+                }
+            }));
+        } catch (IOException ioe) {
+            System.out.println("Comunicación rechazada." + ioe);
+            System.exit(1);
+        }
 
-	private static void handleClient(Socket nsfd) {
-		try (DataInputStream flujoLectura = new DataInputStream(new BufferedInputStream(nsfd.getInputStream()))) {
-			String linea = flujoLectura.readUTF();
+        while (running) {
+            try {
+                Socket nsfd = sfd.accept();
+                System.out.println("Conexión aceptada de: " + nsfd.getInetAddress());
+                new Thread(() -> {
+                    handleClient(nsfd);
+                }).start();
+            } catch (IOException ioe) {
+                System.out.println("Error: " + ioe);
+            }
+        }
+    }
 
-			if (!linea.equals("")) {
-				String fileName = "C:\\Users\\DELL\\Documents\\Plantillas personalizadas de Office\\Clinica_"
-						+ connectionCounter + ".dat";
-				connectionCounter++;
+    private static void handleClient(Socket nsfd) {
+        try (BufferedInputStream bis = new BufferedInputStream(nsfd.getInputStream())) {
+            String fileName = "Clinica_" + connectionCounter + ".dat";
+            connectionCounter++;
 
-				// Guardar el texto recibido en un archivo único para esta conexión
-				try (FileWriter fileWriter = new FileWriter(fileName, true);
-						BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-						PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
-					printWriter.println(linea);
-				} catch (IOException e) {
-					System.out.println("Error al guardar el archivo: " + e.getMessage());
-				}
-
-				System.out.println("Texto recibido y guardado en el archivo: " + fileName);
-			}
-		} catch (IOException ioe) {
-			System.out.println("Error al leer el mensaje del cliente: " + ioe);
-		} finally {
-			try {
-				nsfd.close();
-			} catch (IOException e) {
-				System.out.println("Error al cerrar la conexión: " + e.getMessage());
-			}
-		}
-	}
+            // Guardar el archivo recibido en el servidor
+            try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = bis.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+                System.out.println("Archivo recibido y guardado: " + fileName);
+            } catch (IOException e) {
+                System.out.println("Error al guardar el archivo: " + e.getMessage());
+            }
+        } catch (IOException ioe) {
+            System.out.println("Error al leer el mensaje del cliente: " + ioe);
+        } finally {
+            try {
+                nsfd.close();
+            } catch (IOException e) {
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
 }
